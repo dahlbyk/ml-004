90bd549967952ef7c663dcfe904fa3fa3d33e085
Keith Dahlby <dahlbyk@gmail.com>
2013-11-17 14:04:12 -0600

    Done with ex2, part 2/3

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -20,6 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+J = -(y' * log(sigmoid(X * theta)) + (1 - y') * log(1 - sigmoid(X * theta))) / m;
+
+grad = X' * (sigmoid(X * theta) - y) / m;
 
 
 
