33ece08c3278da5ab3eaef0355cf42b5196474d8
Keith Dahlby <dahlbyk@gmail.com>
2013-11-18 10:59:33 -0600

    Done with ex3, part 2/3

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,6 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+lambda_filter = eye(size(theta)(1));
+lambda_filter(1,1) = 0;
+
+J = -(y' * log(sigmoid(X * theta)) + (1 - y') * log(1 - sigmoid(X * theta))) / m + (theta' * lambda_filter * theta) .* (lambda / (2 * m));
+
+grad = X' * (sigmoid(X * theta) - y) / m + (lambda_filter * theta) .* (lambda / m);
+
 
 
 
